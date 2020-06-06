5edd32e1ca2e02f28fa94ceb72c706e6dcf15d58
Fahri MacBook <fahri-can@web.de>
2020-06-06 18:13:13 +0200

    Create ViewHolder.

---
+++ app/src/main/java/com/example/giphyapiandroom/view/adapter/TrendingViewHolderAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.giphyapiandroom.view.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.giphyapiandroom.databinding.ItemTrendBinding
+
+
+class TrendingViewHolder(
+    val itemTrendingBinding: ItemTrendBinding
+) : RecyclerView.ViewHolder(itemTrendingBinding.root)
