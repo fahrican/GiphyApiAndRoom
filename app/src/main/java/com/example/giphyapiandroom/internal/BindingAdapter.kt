66f2e8962f6fd2183b92d9b268d6f4eb2e89892a
Fahri MacBook <fahri-can@web.de>
2020-06-06 17:25:17 +0200

    Add BindingAdapter for item_trend.xml.

---
+++ app/src/main/java/com/example/giphyapiandroom/internal/BindingAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.giphyapiandroom.internal
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+
+@BindingAdapter("imageUrl")
+fun setImageUrl(imageView: ImageView, url: String?) {
+    Glide.with(imageView)
+        .load(url)
+        .centerCrop()
+        .into(imageView)
+}
