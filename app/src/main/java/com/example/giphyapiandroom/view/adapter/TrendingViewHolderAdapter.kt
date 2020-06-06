1a3f5c2f1099efb66eac36cd02f3e55712a7a386
Fahri MacBook <fahri-can@web.de>
2020-06-06 19:06:18 +0200

    Finish implementing adapter.

--- app/src/main/java/com/example/giphyapiandroom/view/adapter/TrendingViewHolderAdapter.kt
+++ app/src/main/java/com/example/giphyapiandroom/view/adapter/TrendingViewHolderAdapter.kt
@@ -1,9 +1,43 @@
 package com.example.giphyapiandroom.view.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.giphyapiandroom.databinding.ItemTrendBinding
+import com.example.giphyapiandroom.R
+import com.example.giphyapiandroom.databinding.ItemGiphyBinding
+import com.example.giphyapiandroom.model.Data
+
+
+class TrendingAdapter(
+    private val data: ArrayList<Data>
+) : RecyclerView.Adapter<TrendingViewHolder>() {
+
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrendingViewHolder {
+        val itemGiphyBinding: ItemGiphyBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_giphy,
+            parent,
+            false
+        )
+        return TrendingViewHolder(itemGiphyBinding)
+    }
+
+    override fun onBindViewHolder(holder: TrendingViewHolder, position: Int) {
+        holder.itemGiphyBinding.data = data[position]
+    }
+
+    fun setUpData(giphies: List<Data>) {
+        data.clear()
+        data.addAll(giphies)
+        notifyDataSetChanged()
+    }
+}
 
 
 class TrendingViewHolder(
-    val itemTrendingBinding: ItemTrendBinding
-) : RecyclerView.ViewHolder(itemTrendingBinding.root)
+    val itemGiphyBinding: ItemGiphyBinding
+) : RecyclerView.ViewHolder(itemGiphyBinding.root)
