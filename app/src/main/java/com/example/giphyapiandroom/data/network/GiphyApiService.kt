44c76ce6d76dbe515400c89f680fd36a8914adfd
Fahri MacBook <fahri-can@web.de>
2020-06-07 19:46:44 +0200

    Implement service for API request.

---
+++ app/src/main/java/com/example/giphyapiandroom/data/network/GiphyApiService.kt
@@ -0,0 +1,61 @@
+package com.example.giphyapiandroom.data.network
+
+import com.example.giphyapiandroom.internal.BASE_URL
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object GiphyApiService {
+
+    // Here gets the HTTP request logged to Logcat
+    private fun generateInterceptorCallback(): Interceptor {
+        return object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val originalRequest: Request = chain.request()
+                val request = originalRequest.newBuilder().build()
+                return chain.proceed(request)
+            }
+        }
+    }
+
+    // Add Interceptor and connection timeout
+    private fun createOkHttpClient(requestInterceptor: Interceptor): OkHttpClient {
+        val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(requestInterceptor)
+            .addInterceptor(generateInterceptorCallback())
+            .addInterceptor(logger)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun getClient(): GiphyApi {
+        val requestInterceptor = Interceptor { chain ->
+            val url: HttpUrl = chain.request()
+                .url
+                .newBuilder()
+                .build()
+
+            val request: Request = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+
+            return@Interceptor chain.proceed(request)
+        }
+        val okHttpClient = createOkHttpClient(requestInterceptor)
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GiphyApi::class.java)
+    }
+
+}
