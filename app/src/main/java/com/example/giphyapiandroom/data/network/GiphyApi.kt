78907897007f7e0091dea9d0ab6a6f2dc8d322a6
Fahri MacBook <fahri-can@web.de>
2020-06-07 19:53:52 +0200

    Modify interface for API request.

--- app/src/main/java/com/example/giphyapiandroom/data/network/GiphyApi.kt
+++ app/src/main/java/com/example/giphyapiandroom/data/network/GiphyApi.kt
@@ -3,9 +3,16 @@ package com.example.giphyapiandroom.data.network
 import com.example.giphyapiandroom.model.TrendingResult
 import io.reactivex.Flowable
 import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface GiphyApi {
 
     @GET("v1/gifs/trending")
-    fun getTrending(): Flowable<TrendingResult>
+    fun getTrending(
+        @Query("api_key") api_key: String,
+        @Query("limit") limit: String,
+        @Query("rating") rating: String
+    ): Flowable<TrendingResult>
+
 }
