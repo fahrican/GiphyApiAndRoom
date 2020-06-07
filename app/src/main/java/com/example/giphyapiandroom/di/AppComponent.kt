938f175fc03e75bfda753dfa9484678b39ac0bf3
Fahri MacBook <fahri-can@web.de>
2020-06-07 22:54:44 +0200

    Forget to add Singleton annotation.

--- app/src/main/java/com/example/giphyapiandroom/di/AppComponent.kt
+++ app/src/main/java/com/example/giphyapiandroom/di/AppComponent.kt
@@ -2,7 +2,10 @@ package com.example.giphyapiandroom.di
 
 import com.example.giphyapiandroom.repository.TrendingRepository
 import dagger.Component
+import javax.inject.Singleton
 
+
+@Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
 
