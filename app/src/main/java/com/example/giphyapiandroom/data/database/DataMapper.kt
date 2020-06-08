4d5f140495c5cd6552f62edf513e78f1c20e1767
Fahri MacBook <fahri-can@web.de>
2020-06-09 00:11:50 +0200

    Create Mapper class with extension functions.

---
+++ app/src/main/java/com/example/giphyapiandroom/data/database/DataMapper.kt
@@ -0,0 +1,24 @@
+package com.example.giphyapiandroom.data.database
+
+import com.example.giphyapiandroom.model.Data
+import com.example.giphyapiandroom.model.FixedHeightSmallStill
+import com.example.giphyapiandroom.model.Images
+
+
+fun DataEntity.toData() = Data(
+    Images(FixedHeightSmallStill("100", "8021", this.images, "100")),
+    this.title,
+    this.type,
+    this.username
+)
+
+fun List<DataEntity>.toDataList() = this.map { it.toData() }
+
+fun Data.toDataEntity() = DataEntity(
+    images = this.images.fixedHeightSmallStill.url,
+    title = this.title,
+    type = this.type,
+    username = this.username
+)
+
+fun List<Data>.toDataEntityList() = this.map { it.toDataEntity() }
