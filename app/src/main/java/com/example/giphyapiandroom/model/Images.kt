fcf0a411efe26f42102b31d9f36a38a4b6484f7f
Fahri MacBook <fahri-can@web.de>
2020-06-08 23:51:59 +0200

    Remove extra image class.

--- app/src/main/java/com/example/giphyapiandroom/model/Images.kt
+++ app/src/main/java/com/example/giphyapiandroom/model/Images.kt
@@ -3,8 +3,6 @@ package com.example.giphyapiandroom.model
 import com.google.gson.annotations.SerializedName
 
 data class Images(
-    @SerializedName("fixed_height")
-    val fixedHeight: FixedHeight,
     @SerializedName("fixed_height_small_still")
     val fixedHeightSmallStill: FixedHeightSmallStill
 )
