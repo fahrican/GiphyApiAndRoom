4002dc8d3dffb96b96255a264c8a9034be55da10
Fahri MacBook <fahri-can@web.de>
2020-06-09 23:33:13 +0200

    Create DAO for database.

---
+++ app/src/main/java/com/example/giphyapiandroom/data/database/DataDao.kt
@@ -0,0 +1,18 @@
+package com.example.giphyapiandroom.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.reactivex.Single
+
+
+@Dao
+interface DataDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertData(data: List<DataEntity>)
+
+    @Query("SELECT * from data")
+    fun queryData(): Single<List<DataEntity>>
+}
