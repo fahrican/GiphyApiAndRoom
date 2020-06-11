fbdb554c42f84b0f4aa53065af42bf32df6becb1
Fahri MacBook <fahri-can@web.de>
2020-06-11 22:15:26 +0200

    Implement application class.

--- app/src/main/java/com/example/giphyapiandroom/GiphyApplication.kt
+++ app/src/main/java/com/example/giphyapiandroom/GiphyApplication.kt
@@ -1,6 +1,21 @@
 package com.example.giphyapiandroom
 
 import android.app.Application
+import com.example.giphyapiandroom.data.database.TrendingDatabase
 
 class GiphyApplication : Application() {
+
+    companion object {
+        lateinit var instance: GiphyApplication
+        lateinit var database: TrendingDatabase
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        database = TrendingDatabase.invoke(this)
+    }
 }
