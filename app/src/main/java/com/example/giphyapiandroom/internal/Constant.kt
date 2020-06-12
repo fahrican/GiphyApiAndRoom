a51b43dfc4878bd5fb04cad1689129f40f6ef563
Fahri MacBook <fahri-can@web.de>
2020-06-12 13:41:25 +0200

    Make integer to String.

--- app/src/main/java/com/example/giphyapiandroom/internal/Constant.kt
+++ app/src/main/java/com/example/giphyapiandroom/internal/Constant.kt
@@ -3,5 +3,5 @@ package com.example.giphyapiandroom.internal
 
 const val BASE_URL = "https://api.giphy.com/"
 const val DATABASE_NAME = "trending.db"
-const val LIMIT = 25
+const val LIMIT = "25"
 const val RATING = "G"
