6cf4242068453ee3a01d66161835d50337ab7f5b
Fahri MacBook <fahri-can@web.de>
2020-06-13 20:00:27 +0200

    Remove unused class.

--- app/src/main/java/com/example/giphyapiandroom/model/Images.kt
+++ app/src/main/java/com/example/giphyapiandroom/model/Images.kt
@@ -3,8 +3,6 @@ package com.example.giphyapiandroom.model
 import com.squareup.moshi.Json
 
 data class Images(
-    @field:Json(name = "fixed_height")
-    val fixedHeight: FixedHeight,
     @field:Json(name = "fixed_height_small_still")
     val fixedHeightSmallStill: FixedHeightSmallStill
 )
