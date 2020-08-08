c8e0b497404a2c8c8e12b9f602d9bd487cb24666
Fahri MacBook <fahri-can@web.de>
2020-08-09 01:37:14 +0200

    Adjustments to model because data in the backend has changed.

--- app/src/main/java/com/example/giphyapiandroom/internal/Constant.kt
+++ app/src/main/java/com/example/giphyapiandroom/internal/Constant.kt
@@ -4,4 +4,4 @@ package com.example.giphyapiandroom.internal
 const val BASE_URL = "https://api.giphy.com/"
 const val DATABASE_NAME = "trending.db"
 const val LIMIT = "25"
-const val RATING = "G"
+const val RATING = "g"
